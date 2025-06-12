2915a3b2e3131f86a4d54aecabeafbd24a58bdcb
Reed <maybe.ridho@gmail.com>
2025-06-12 15:08:56 +0700

    dashboard

--- src/com/sisehat/view/DashboardView.java
+++ src/com/sisehat/view/DashboardView.java
@@ -21,14 +21,25 @@ public class DashboardView extends JPanel {
         welcomeLabel = new JLabel("Selamat Datang!", SwingConstants.CENTER);
         welcomeLabel.setFont(new Font("Arial", Font.BOLD, 28));
 
-        startDiagnosisButton = new JButton("Mulai Diagnosa Baru");
+        // load icons
+        ImageIcon diagnosisIcon = new ImageIcon(getClass().getResource("/com/sisehat/view/assets/diagnosisIcon.png"));
+        ImageIcon profileIcon = new ImageIcon(getClass().getResource("/com/sisehat/view/assets/profileIcon.png"));
+        ImageIcon faskesIcon = new ImageIcon(getClass().getResource("/com/sisehat/view/assets/faskesIcon.png"));
+
+        startDiagnosisButton = new JButton("Mulai Diagnosa Baru", diagnosisIcon);
         startDiagnosisButton.setFont(new Font("Arial", Font.PLAIN, 18));
+        startDiagnosisButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        startDiagnosisButton.setVerticalTextPosition(SwingConstants.BOTTOM);
 
-        profileButton = new JButton("Profil & Riwayat Diagnosa");
+        profileButton = new JButton("Profil & Riwayat Diagnosa", profileIcon);
         profileButton.setFont(new Font("Arial", Font.PLAIN, 18));
+        profileButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        profileButton.setVerticalTextPosition(SwingConstants.BOTTOM);
 
-        faskesButton = new JButton("Daftar Fasilitas Kesehatan");
+        faskesButton = new JButton("Daftar Fasilitas Kesehatan", faskesIcon);
         faskesButton.setFont(new Font("Arial", Font.PLAIN, 18));
+        faskesButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        faskesButton.setVerticalTextPosition(SwingConstants.BOTTOM);
 
         this.add(welcomeLabel, gbc);
         this.add(startDiagnosisButton, gbc);
